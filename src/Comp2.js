67750e9d28c123ef66b968e831d2946a24aa830a
Narek1444 <nareksargsyan950@gmail.com>
2022-07-10 21:42:57 +0400

    verj fixes #1

--- src/Comp2.js
+++ src/Comp2.js
@@ -5,6 +5,7 @@ const arr = [1, 2, 3, 4, 5, 6];
 const Comp1 = () => {
     return (
         <div>
+            
             {arr.map(item => {
                 return  <p key={item}>{item}</p>
             })}
